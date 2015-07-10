adf257e9adabe2aecd0c9bd65ab63f7c56be4512
ChefIronBelly <werthmation@gmail.com>
2015-07-10 12:25:10 -0400

    added /dev/null

--- net/home/bin/catssh.sh
+++ net/home/bin/catssh.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-# catssh it (chef 2015)
+# catssh (chef 2015)
 # requires: ssh
-# usage: catssh $FILE $NAME@SERVER /$PATH/
-# example:  kill.la.kill.s01e20.mp4 root@192.168.0.52 /storage/tvshows/
+# usage: catssh $FILE $NAME@SERVER /$PATH/ 
+# example:  kill.la.kill.s01e20.mp4 | ssh root@192.168.0.52 cat ">" /storage/tvshows/
 
-cat $1 | ssh -v $2 cat ">" $3$1
+cat $1 | ssh $2 cat ">" $3$1 2>/dev/null
 echo "copy" $1 ">" $2 "complete"
