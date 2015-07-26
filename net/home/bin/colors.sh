eec6eca4062843c7634ecb16de5df05411221312
ChefIronBelly <werthmation@gmail.com>
2015-07-26 09:38:35 -0400

    adds and tweaks

---
+++ net/home/bin/colors.sh
@@ -0,0 +1,14 @@
+#!/usr/pkg/bin/bash
+ 
+echo
+ 
+for n in '0' '1' '2' '3' '4' '5' '6' \
+           '7' '8' '9' '10' '11' '12' '13';
+  do
+  for BG in 47m 43m 46m 42m 45m 41m 44m;
+    do echo -ne "$EINS\033[$BG        \033[0m";
+  done
+  echo;
+done
+
+echo
