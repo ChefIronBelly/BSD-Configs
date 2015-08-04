fea1bc6fb5dbea5d51b771dbb221581a95695a7c
ChefIronBelly <ChefIronBelly@users.noreply.github.com>
2015-08-04 10:32:51 -0400

    Delete status.sh

--- net/home/bin/status.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-#vol="vol/pcm: $( ( mixer -s vol 2> /dev/null || echo - ) | cut -d ':' -f 2 )/$( ( mixer -s pcm 2> /dev/null || echo - ) | cut -d ':' -f 2 )"
-ip="ip: $( if_ip.sh )"
-time=$(date +" %a, %b %d %I:%M")
-
-echo -n $ip - $time | osd_cat -p top -A center -d 15 -s 0 -c '#eeeeee' -f '-*-dejavu sans mono-*-*-*-*-14-*-*-*-*-*-*-*'
-
