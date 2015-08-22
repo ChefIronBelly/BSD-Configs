7dae0b0559e7475b8f2fc1547468c60c489bc8a5
ChefIronBelly <werthmation@gmail.com>
2015-08-22 14:35:55 -0400

    adds and tweaks

--- net/home/bin/dmenu_go.sh
+++ net/home/bin/dmenu_go.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-dmenu_run -fn 'DejaVuSansMono-14' -nb '#2d2d2d' -nf '#d3d0c8' -sb '#d3d0c8' -sf '#2d2d2d'
+dmenu_run -fn 'Monaco-14' -nb '#b9b9b9' -nf '#101010' -sb '#101010' -sf '#b9b9b9'
