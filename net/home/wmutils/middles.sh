80be4986f48e084c22eb8ce9c4185579beddf367
ChefIronBelly <chef@werthmation.com>
2015-08-24 16:49:57 -0400

    added fudge factor

--- net/home/wmutils/middles.sh
+++ net/home/wmutils/middles.sh
@@ -13,12 +13,12 @@ X=0
 Y=0
 
 case $1 in
-    y) X=$((SW/4 - W/2 - BW))
-        Y=$((SH/2 - H/2 - BW));;
+    y) X=$((SW/4 - W/2 - BW + 40))
+       Y=$((SH/2 - H/2 - BW));;
     g) X=$((SW/2 - W/2 - BW))
-        Y=$((SH/2 - H/2 - BW)) ;;
-    u) X=$((SW/2 + W/4 - BW*2))
-        Y=$((SH/2 - H/2 - BW));;    
+       Y=$((SH/2 - H/2 - BW)) ;;
+    u) X=$((SW/2 + W/4 - BW - 80))
+       Y=$((SH/2 - H/2 - BW));;    
 esac
 
 wtp $X $Y $W $H $CUR
