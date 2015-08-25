e44e9a98fd35a4f2e205814a6696435da8c4e747
ChefIronBelly <werthmation@gmail.com>
2015-08-25 07:48:53 -0400

    added offset variables

--- net/home/wmutils/middles.sh
+++ net/home/wmutils/middles.sh
@@ -9,15 +9,18 @@ BW=$(wattr b $CUR)
 W=$(wattr w $CUR)
 H=$(wattr h $CUR)
 
+offlm=20
+offrm=90
+
 X=0
 Y=0
 
 case $1 in
-    y) X=$((SW/4 - W/2 - BW + 20))
+    y) X=$((SW/4 - W/2 - BW + ${offlm}))
        Y=$((SH/2 - H/2 - BW));;
     g) X=$((SW/2 - W/2 - BW))
        Y=$((SH/2 - H/2 - BW)) ;;
-    u) X=$((SW/2 + W/4 - BW - 90))
+    u) X=$((SW/2 + W/4 - BW - ${offrm}))
        Y=$((SH/2 - H/2 - BW));;    
 esac
 
