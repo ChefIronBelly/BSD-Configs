2c090da4c410c96a37da0d47a6a6315b70552502
ChefIronBelly <werthmation@gmail.com>
2015-10-02 09:42:46 -0400

    adds and tweaks

--- net/home/bin/nbinfo.sh
+++ net/home/bin/nbinfo.sh
@@ -16,64 +16,76 @@ f2="\e[0;37m"
 wms=( 2bwm 2wm 9wm aewm afterstep ahwm alopex amiwm antiwm awesome blackbox bspwm catwm clfswm ctwm cwm dminiwm dragonflywm dwm echinus \
     euclid-wm evilpoison evilwm fluxbox flwm fvwm-crystal goomwwm hcwm herbstluftwm i3 icewm jwm karmen larswm lwm matwm2 mcwm monsterwm \
     musca notion nwm olwm openbox oroborus pekwm ratpoison sapphire sawfish sscrotwm sithwm smallwm snapwm spectrwm stumpwm subtle swm tfwm tinywm tritium twm \
-    uwm vtwm w9wm weewm wind windowlab wm2 wmaker wmfs wmii wmx xfwm4 xmonad xoat yeahwm )
+    uwm vtwm w9wm weewm wind windowlab wm2 wmaker wmfs wmii wmutils wmx xfwm4 xmonad xoat yeahwm )
 
 color-echo() {  # print with colors
-	echo -e "$blu$1: $rst$2"
+	echo -e "$grn$1: $rst$2"
 }
 
 print-kernel() {
-	color-echo 'Kernel' '-------- '"$(uname -mrs)"
+	color-echo 'KERNEL' '   '"$(uname -mrs)"
 }
 
 print-shell() {
 	fshell=$(echo $SHELL | cut -d '/' -f 5)
-	color-echo 'Shell' '--------- '"$fshell"
+	color-echo 'SHELL' '    '"$fshell"
 }
 
 print-term() {
-	color-echo 'Term' '---------- '"$TERM"
+	color-echo 'TERM' '     '"$TERM"
 }
 
 print-cpu() {
 	cpu=$(awk -F': ' '/model name/ {gsub("\\(R\\)",""); gsub("\\(TM\\)",""); print $2; exit}' /proc/cpuinfo)
-	color-echo 'CPU' '----------- '"$cpu"
+	color-echo 'CPU' '      '"$cpu"
 }
 
 print-packages() {
 	pkgs=$(pkg_info | wc -l | awk '{sub(" ", "");print $1}')
-	color-echo 'Packages' '------ '"$pkgs"
+	color-echo 'PACKAGES' ' '"$pkgs"
 }
 
 print-wm() {
     for wm in ${wms[@]}; do          # pgrep through wmname array
         pid=$(pgrep -x -u $USER $wm)		# if found, this wmname has running process
         if [[ "$pid" ]]; then
-            color-echo 'WM' ' ----------- '"$wm"
+            color-echo 'WM' '       '"$wm"
 	        break
         fi
     done
-    color-echo 'WM' '------------ ''null, wmutils engaged'
+#    color-echo 'WM' '------------ ''Not Found'
 }
            
 print-font() {
     fontstr=$(xrdb -query 2>/dev/null | grep '*faceName:')
     font=$(echo $fontstr | awk -F: '{ print $3 }')
-    [[ $font != "" ]] && color-echo Font '---------- '"$font"
+    [[ $font != "" ]] && color-echo FONT '     '"$font"
 }
 
 print-distro() {
 	PRETTY_NAME=$(uname)
 	if [[ -n "$PRETTY_NAME" ]]; then
-        color-echo 'OS' '------------ '"$PRETTY_NAME"
+        color-echo 'OS' '       '"$PRETTY_NAME"
 	else
-        color-echo 'OS' '------------ '"not found"
+        color-echo 'OS' '       '"not found"
 	fi
 }
 
 print-gitdir() {
 	gitdir="/ChefIronBelly"
-	color-echo 'Github' '-------- '"$gitdir"
+	color-echo 'Github' '   '"$gitdir"
+}
+
+print-colors() {
+	COLORS=('▓▒░' '▓▒░' '▓▒░' '▓▒░' '▓▒░' '▓▒░' '▓▒░' '▓▒░')
+	for f in `seq 0 7`; do
+        echo -ne "\033[m\033[$(($f+30))m ${COLORS[$f]} " # normal colors
+	done
+	echo
+	for f in `seq 0 7`; do
+        echo -ne "\033[m\033[1;$(($f+30))m ${COLORS[$f]} " # bold colors
+	done
+	echo -e "$rst\n"
 }
 
 clear
@@ -89,5 +101,6 @@ echo
 print-kernel
 print-cpu
 echo
-print-gitdir
-echo
+#print-gitdir
+#echo
+print-colors
