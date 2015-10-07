9b077ce032f7d2e99fbfce186ae8dc920bb8823b
ChefIronBelly <werthmation@gmail.com>
2015-10-07 18:58:17 -0400

    adds and tweaks

---
+++ net/home/bin/roller.sh
@@ -0,0 +1,29 @@
+#!/usr/pkg/bin/mksh
+
+EXCLUDES="/etc/bbq_excludes"
+
+if ! [ -f $EXCLUDES ] ; then
+cat > "$EXCLUDES" << EOF
+- /dev/*
+- /cdrom/*
+- /mnt/*
+- /proc/*
+- /tmp/*
+- /home/chef/Pictures
+- /home/chef/Video
+- /home/chef/Downloads
+- /home/chef/Music
+- /home/chef/Source
+- /home/chef/.cache
+EOF
+chmod 666 "$EXCLUDES"
+fi
+
+echo "I: Copying File System, please wait"
+rsync -av / /tmp/target/ --exclude-from="$EXCLUDES" >/dev/null 2>&1
+echo "I: Success. File System copied."
+echo
+echo "I: Compressing File System. please wait"
+cd /tmp/target && sudo tar -czf /tmp/netbsd7.tar.gz .
+echo "I: File System Compressed."
+echo
