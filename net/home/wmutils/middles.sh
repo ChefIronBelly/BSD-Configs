11c6d1c44e2095861bbff3a121a9abca6107d250
ChefIronBelly <chef@werthmation.com>
2015-10-30 19:53:33 -0400

    adds and tweaks

--- net/home/wmutils/middles.sh
+++ net/home/wmutils/middles.sh
@@ -15,12 +15,21 @@ offrm=100
 X=0
 Y=0
 
+# it's pretty simple, but anyway...
+usage() {
+    echo "usage: $(basename $0) <left|middle|right>"
+    exit 1
+}
+
+# exit if no argument given
+test -z "$1" && usage
+
 case $1 in
-    y) X=$((SW/4 - W/2 - BW + ${offlm}))
+    left) X=$((SW/4 - W/2 - BW + ${offlm}))
        Y=$((SH/2 - H/2 - BW));;
-    g) X=$((SW/2 - W/2 - BW))
+    middle) X=$((SW/2 - W/2 - BW))
        Y=$((SH/2 - H/2 - BW)) ;;
-    u) X=$((SW/2 + W/4 - BW - ${offrm}))
+    right) X=$((SW/2 + W/4 - BW - ${offrm}))
        Y=$((SH/2 - H/2 - BW));;    
 esac
 
