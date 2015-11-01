0477246c7c582da32a76615a44603e5d9ea4727e
ChefIronBelly <werthmation@gmail.com>
2015-11-01 17:39:42 -0500

    adds and tweaks

--- net/home/bin/statusbar.sh
+++ net/home/bin/statusbar.sh
@@ -75,7 +75,7 @@ group4() {
     echo $group4
 }
 
-echo %{l}%{B${a}${XCOL1}%{A:xterm -e cmus:}%{F${a}${XCOL3}}"    "%{A}%{F-}%{F${a}${XCOL5}}$(cur)%{F-}%{c}%{F${a}${XCOL5}}    $(group1)%{F-} %{F${a}${XCOL5}}$(group2)%{F-} %{F${a}${XCOL5}}$(group3)%{F-} %{F${a}${XCOL5}}$(group4)%{F-}%{r}%{F${a}${XCOL3}}"   "%{F-}$(ip)%{F${a}${XCOL3}}"    "%{F-}$(vol)%{F${a}${XCOL3}}"     "%{F-}$(dateclock)%{F${a}${XCOL3}}"    "%{F-}$(clock)%{F${a}${XCOL3}}"     "%{F-}
+echo %{l}%{B${a}${XCOL1}%{F${a}${XCOL3}}%{A:xterm -e cmus:}"    "%{A}%{F-}%{F${a}${XCOL5}}$(cur)%{F-}%{c}%{F${a}${XCOL5}}    $(group1)%{F-} %{F${a}${XCOL5}}$(group2)%{F-} %{F${a}${XCOL5}}$(group3)%{F-} %{F${a}${XCOL5}}$(group4)%{F-}%{r}%{F${a}${XCOL3}}"   "%{F-}$(ip)%{F${a}${XCOL3}}"    "%{F-}$(vol)%{F${a}${XCOL3}}"     "%{F-}$(dateclock)%{F${a}${XCOL3}}"    "%{F-}$(clock)%{F${a}${XCOL3}}"     "%{F-}
 }
 
 while true
