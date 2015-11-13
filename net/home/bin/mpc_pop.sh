0952af8e7e6101379d69e9092d8f6d1520f211a3
ChefIronBelly <werthmation@gmail.com>
2015-11-13 15:22:28 -0500

    adds and tweaks

--- net/home/bin/mpc_pop.sh
+++ net/home/bin/mpc_pop.sh
@@ -2,8 +2,8 @@
 
 mus=$(mpc -f %artist% current)
 sng=$(mpc -f %title% current)
-if [ -z $mus ]; then
-echo "Select a song"
+if [[ $mus ]]; then
+echo "[ Select a song. or update mp3 tags... ]"
 else
-echo "Now: " $mus - $sng
+echo "[ Now: " $mus - $sng "]"
 fi
