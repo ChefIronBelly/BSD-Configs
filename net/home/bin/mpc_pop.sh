6d3d4164732c86cd4bcaa22dc6f2ae60bdcf44ea
ChefIronBelly <werthmation@gmail.com>
2015-11-14 10:15:50 -0500

    adds and tweaks

--- net/home/bin/mpc_pop.sh
+++ net/home/bin/mpc_pop.sh
@@ -2,8 +2,9 @@
 
 mus=$(mpc -f %artist% current)
 sng=$(mpc -f %title% current)
+
 if [[ $mus ]]; then
-echo "[ Select a song. or update mp3 tags... ]"
+echo "[Select a song or update ID3 tags...]"
 else
-echo "[ Now: " $mus - $sng "]"
+echo "[Now: " $mus - $sng"]"
 fi
--- net/home/etc/swm/config.h
+++ net/home/etc/swm/config.h
@@ -6,9 +6,9 @@
 #define OUTER		1		/*  outer border width */
 
 /* colors */
-#define FOCUSCOL	0x18191A
-#define UNFOCUSCOL	0x111213
-#define OUTERCOL	0x000000
+#define FOCUSCOL	0x4f5b66
+#define UNFOCUSCOL	0x343d46
+#define OUTERCOL	0x343d46
 
 /* resize and move by mouse? */
 #define ENABLE_MOUSE
