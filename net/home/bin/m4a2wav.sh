8038c2675c1f8163810431d3285b105baa50ce5f
ChefIronBelly <werthmation@gmail.com>
2015-11-15 08:20:39 -0500

    adds and tweaks

--- net/home/bin/m4a2wav.sh
+++ net/home/bin/m4a2wav.sh
@@ -1,7 +1,7 @@
 #!/usr/pkg/bin/mksh
 LAMEOPTS="-p -o -v -V 5 -h"
 
-for FILE in *.wav ; do
-    OUTNAME=`basename "$FILE" .wav`.mp3
+for FILE in *.m4a ; do
+    OUTNAME=`basename "$FILE" .m4a`.mp3
     lame $LAMEOPTS "$FILE" "$OUTNAME"
 done
