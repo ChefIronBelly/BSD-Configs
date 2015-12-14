a7238a6e5471611982336259cb6fc71ef80dbc14
ChefIronBelly <werthmation@gmail.com>
2015-12-14 09:35:43 -0500

    adds and tweaks

--- net/home/bin/volume.sh
+++ net/home/bin/volume.sh
@@ -6,11 +6,11 @@ volume=`mixerctl $device | cut -d "," -f 2`
 X=0
 
 case $1 in
-	9 )
-		X=$((${volume} + ${inc}))
-		mixerctl -w $device=$X,$X 2>/dev/null;;
-	8 )
+	- )
 		X=$((${volume} - ${inc}))
+		mixerctl -w $device=$X,$X 2>/dev/null;;
+	+ )
+		X=$((${volume} + ${inc}))
 		mixerctl -w $device=$X,$X 2>/dev/null;;
 	0 )
 		if [ ${volume} != "0" ]; then
