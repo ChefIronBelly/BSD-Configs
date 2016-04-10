34180a61aa681b6e8e1f77662b87d06bedb9508c
User Chef <werthmation@gmail.com>
2016-04-10 10:08:23 -0400

    adds and tweaks

--- free/home/bin/volume.sh
+++ free/home/bin/volume.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
-device=outputs.master
+device=vol
 inc=10
-volume=`mixerctl $device | cut -d "," -f 2`
+volume=`mixer $device | cut -d ":" -f 2`
 X=0
 
 case $1 in
 	- )
 		X=$((${volume} - ${inc}))
-		mixerctl -w $device=$X,$X 2>/dev/null;;
+		mixer -s $device $X:$X 2>/dev/null;;
 	+ )
 		X=$((${volume} + ${inc}))
-		mixerctl -w $device=$X,$X 2>/dev/null;;
+		mixer -s $device $X:$X 2>/dev/null;;
 	0 )
 		if [ ${volume} != "0" ]; then
-			mixerctl -w $device=0,0
+			mixer -s $device 0,0
 		else
-			mixerctl -w $device=208,208
+			mixer -s $device 208,208
 		fi
 		;;
 esac
