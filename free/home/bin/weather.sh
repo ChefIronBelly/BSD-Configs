19cb854b4022ce2b5cc7090a6b166c2a66af1c57
ChefIronBelly <werthmation@gmail.com>
2016-04-26 10:01:34 -0400

    adds and tweaks

--- free/home/bin/weather.sh
+++ free/home/bin/weather.sh
@@ -1,5 +1,5 @@
 #!/usr/local/bin/bash
-
+# original source https://github.com/blamonet/bash-weather
 # jsonval credit: https://gist.github.com/cjus/1047794
 jsonval () {
 temp=`echo $haystack | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w $needle`
