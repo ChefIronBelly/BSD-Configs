832e2a34ad0e2f1cf5c0ee3efcb1e125cbb467d3
ChefIronBelly <werthmation@gmail.com>
2016-08-23 17:01:11 -0400

    adds and tweaks

--- free/home/bin/gind.sh
+++ free/home/bin/gind.sh
@@ -7,23 +7,23 @@ group3=""
 group4=""
 
 if [ -f $FSDIR/1 ];
-    then group1="+"
-    else group1="-"
+    then group1="1"
+    else group1="0"
 fi
 
 if [ -f $FSDIR/2 ];
-    then group2="+"
-    else group2="-"
+    then group2="1"
+    else group2="0"
 fi
 
 if [ -f $FSDIR/3 ];
-    then group3="+"
-    else group3="-"
+    then group3="1"
+    else group3="0"
 fi
 
 if [ -f $FSDIR/4 ];
-    then group4="+"
-    else group4="-"
+    then group4="1"
+    else group4="0"
 fi
 
-echo "$group4""$group3""$group2""$group1"
+echo "0x""$group4""$group3""$group2""$group1"
--- free/home/bin/usbmnt.sh
+++ free/home/bin/usbmnt.sh
@@ -2,5 +2,8 @@
 # dev/sd0e or sd4e on my machines #netbsd
 # dev/da4 on my machines #netbsd
 
+# fdisk ada4
+
 # sudo mount_msdos /dev/sd4e /mnt #netbsd
 sudo mount_msdosfs /dev/da4s1  /mnt/usb #freebsd
+#sudo mount -t ntfs -o ro /dev/ada4s1 /mnt #ntfs
