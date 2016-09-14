ca6531c559e2b72fe00192a45db156502379284e
ChefIronBelly <werthmation@gmail.com>
2016-09-14 08:34:45 -0400

    adds and tweaks

--- open/home/bin/status.sh
+++ open/home/bin/status.sh
@@ -4,8 +4,8 @@
 mem="mem: $(top -n 1 | head -n 5 | tail -n 1 | awk '{print $3}')"
 vol="vol:$(volumebar.sh)"
 ip="ip: $( if_ip.sh )"
-mail="mail: $(gmail.sh)"
+#mail="mail: $(gmail.sh)"
 time=$(date +" %a, %b %d %I:%M")
 
-echo -n $time "|" $mem "|" $ip "|" $mail "|" $vol | osd_cat -p top -A center -d 5 -s 0 -c '#eeeeee' -f '-*-ohsnap.icons-medium-r-*-*-14-*-*-*-*-*-*-*'
+echo -n $time "|" $mem "|" $ip "|" $vol | osd_cat -p top -A center -d 5 -s 0 -c '#eeeeee' -f '-*-ohsnap.icons-medium-r-*-*-14-*-*-*-*-*-*-*'
 
