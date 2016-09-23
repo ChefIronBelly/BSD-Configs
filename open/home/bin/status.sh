d870bfa89a505cb6bc430b94856a5337c5b8c26c
ChefIronBelly <werthmation@gmail.com>
2016-09-23 09:15:40 -0400

    adds and tweaks

--- open/home/bin/status.sh
+++ open/home/bin/status.sh
@@ -2,12 +2,20 @@
 
 . $HOME/.wmrc
 
-#playing="Now: $(cmus-info)"
-mem="mem: $(top -n 1 | head -n 5 | tail -n 1 | awk '{print $3}')"
-vol="vol:$(volumebar.sh)"
-ip="ip: $( if_ip.sh )"
-#mail="mail: $(gmail.sh)"
-time=$(date +" %a, %b %d %I:%M")
-cur=$(mpc_pop.sh)
+  #playing="Now: $(cmus-info)"
+  mem="mem: $(top -n 1 | head -n 5 | tail -n 1 | awk '{print $3}')"
+  vol="vol:$(volumebar.sh)"
+  ip="ip: $( if_ip.sh )"
+  #mail="mail: $(gmail.sh)"
+  time=$(date +" %a, %b %d %I:%M")
+  cur=$(mpc_pop.sh)
 
-echo -n $time "|" $mem "|" $ip "|" $vol $cur | osd_cat -p top -A center -d 5 -s 0 -c '#f8f8f2' -f $FONT
+  current=""
+  if mpc status | grep -q playing; then
+    current="$(mpc current --format "%artist% - %title% $(date -d@$(mpdtime) +%-M:%S)/%time%")"
+    [[ ${#current} -gt 80 ]] && current="${current:0:10}...${current:(-70)}"
+  elif mpc status | grep -q paused; then
+    current="[paused] "
+  fi
+
+  echo -n $time "|" $mem "|" $ip "|" $vol "|" $current | osd_cat -p top -A center -d 5 -s 0 -c '#f8f8f2' -f $FONT
