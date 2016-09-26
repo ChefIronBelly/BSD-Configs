f7a6d9ca1a5038da7a9b11d2ecc92655f3bf38f8
ChefIronBelly <chefiron@teknik.io>
2016-09-25 20:04:45 -0400

    adds and tweaks

--- open/home/bin/status.sh
+++ open/home/bin/status.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
+# ~/bin/status.sh
+#
 
 . $HOME/.wmrc
 
-  #playing="Now: $(cmus-info)"
   mem="mem: $(top -n 1 | head -n 5 | tail -n 1 | awk '{print $3}')"
   vol="vol:$(volumebar.sh)"
   ip="ip: $( if_ip.sh )"
   #mail="mail: $(gmail.sh)"
   time=$(date +" %a, %b %d %I:%M")
-  cur=$(mpc_pop.sh)
-
   current=""
+  
   if mpc status | grep -q playing; then
-    current="$(mpc current --format "%artist% - %title% $(date -d@$(mpdtime) +%-M:%S)/%time%")"
+    current="| ""$(mpc current --format "%artist% - %title% $(date -d@$(mpdtime) +%-M:%S)/%time%")"
     [[ ${#current} -gt 80 ]] && current="${current:0:10}...${current:(-70)}"
   elif mpc status | grep -q paused; then
-    current="[paused] "
+    current="| [paused] "
   fi
 
-  echo -n $time "|" $mem "|" $ip "|" $vol "|" $current | osd_cat -p top -A center -d 5 -s 0 -c '#f8f8f2' -f $FONT
+  echo -n $time "|" $mem "|" $ip "|" $vol $current | osd_cat -p top -A center -d 5 -s 0 -c '#f8f8f2' -f $FONT
