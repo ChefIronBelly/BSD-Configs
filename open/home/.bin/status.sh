c9715a3a2162f820fcb8a5db3419e466cef5bc90
ChefIronBelly <chefiron@teknik.io>
2016-11-22 06:09:55 -0500

    adds and tweaks

--- open/home/.bin/status.sh
+++ open/home/.bin/status.sh
@@ -7,7 +7,7 @@
 #mem="mem: $(top -n 1 | head -n 7 | tail -n 1 | awk '{print $3}')"
 memtotal="$(($(sysctl -n hw.physmem) / 1024 / 1024))"
 memused="$(($(vmstat | awk 'END {printf $4}') / 1024))"
-mem="mem:${memused}MB / ${memtotal}MB"
+mem="mem:${memused}MB/${memtotal}MB"
 vol="vol:$(volumebar.sh)"
 ip="ip:$( if_ip.sh )"
 #mail="mail: $(gmail.sh)"
@@ -21,4 +21,4 @@ elif mpc status | grep -q paused; then
   current="| paused "
 fi
 
-echo -n $time "|" $mem "|" $ip "|" $vol | osd_cat -p top -A center -d 10 -s 0 -c '#ffffff' -f $FONT
+echo -n $time "|" $mem "|" $ip "|" $vol $current | osd_cat -p top -A center -d 10 -s 0 -c '#ffffff' -f $FONT
