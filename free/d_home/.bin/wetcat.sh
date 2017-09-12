d1a246402a70da2f69fc2ca5cce60645b2f3a283
ChefIronBelly <werthmation@gmail.com>
2017-09-12 15:14:44 -0400

    adds and tweaks

--- free/d_home/.bin/wetcat.sh
+++ free/d_home/.bin/wetcat.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 # ~/bin/wetcat.sh
 #
-
 . $HOME/.wmrc
-. $HOME/.colors/zenblue
-
 cw="$(weather.sh)"
 
-echo -n $cw | osd_cat -p top -A center -d 10 -s 0 -o 5 -c '#ffffff' -f $FONT
+echo -n $cw | osd_cat -p top -A center -d 10 -s 0 -o 2 -c '#cfb9a8' -f $FONT
