cd010392f57eb9bebc49fccbeb242c5dd98210fc
ChefIronBelly <werthmation@gmail.com>
2017-09-15 07:49:38 -0400

    adds and tweaks

---
+++ free/d_home/.bin/lmnt.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+# dev/sd0e or sd4e on my machines #netbsd
+# dev/da4 on my machines #netbsd
+
+# sudo mount_msdos /dev/sd4e /mnt #netbsd
+sudo mount_msdosfs /dev/da4s1  /mnt/lacie #freebsd
