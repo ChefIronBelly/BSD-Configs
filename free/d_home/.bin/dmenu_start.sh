6b1c409a7e8242339836d789aa3462d6e60d9d8d
ChefIronBelly <werthmation@gmail.com>
2017-10-11 10:36:15 -0400

    adds and tweaks

--- free/d_home/.bin/dmenu_start.sh
+++ free/d_home/.bin/dmenu_start.sh
@@ -2,4 +2,4 @@
 
 . $HOME/.wmrc
 
-dmenu_run -i $DMENU_FN $DMENU_NB $DMENU_NF $DMENU_SF $DMENU_SB
+dmenu_run -b -i $DMENU_FN $DMENU_NB $DMENU_NF $DMENU_SF $DMENU_SB
--- free/d_home/.bin/dmenu_xset.sh
+++ free/d_home/.bin/dmenu_xset.sh
@@ -3,5 +3,5 @@
 
 . $HOME/.wmrc
 
-selection="$(dmenu -p "Color: $@")"
+selection=`echo "Color" | dmenu -b $DMENU_FN $DMENU_NB $DMENU_NF $DMENU_SF $DMENU_SB -p "enter a value..."`
 xsetroot -solid "#$selection"
