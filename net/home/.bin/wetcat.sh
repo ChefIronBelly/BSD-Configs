98c294d7118773b9db5582a69acf86261595e04e
ChefIronBelly <werthmation@gmail.com>
2018-05-30 06:52:06 -0400

    adds and tweaks

--- net/home/.bin/wetcat.sh
+++ net/home/.bin/wetcat.sh
@@ -4,4 +4,5 @@
 . $HOME/.wmrc
 cw="$(weather.sh)"
 
-echo -n $cw | osd_cat -p top -A center -d 10 -s 0 -o 2 -c '#cfb9a8' -f $FONT
+#echo -n $cw | osd_cat -p top -A center -d 10 -s 0 -o 2 -c '#cfb9a8' -f $FONT
+pop.sh $cw
