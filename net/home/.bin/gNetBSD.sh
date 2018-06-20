eaa34889fcf06b255d5795f471a29432b91d28bd
ChefIronBelly <werthmation@gmail.com>
2018-06-20 08:53:04 -0400

    adds and tweaks

---
+++ net/home/.bin/gNetBSD.sh
@@ -0,0 +1,15 @@
+#!/usr/pkg/bin/bash
+#
+# Get NetBSD daily sets
+#
+# depends: curl and common sense
+
+set -e
+set -x
+
+mkdir ~/HEAD/$1 && cd $_
+
+for file in base comp etc kern-GENERIC modules xbase xcomp xetc xfont xserver
+do
+    curl -LO http://nyftp.netbsd.org/pub/NetBSD-daily/HEAD/$1/amd64/binary/sets/${file}.tgz
+done
---
+++ net/home/.bin/uNetBSD.sh
@@ -0,0 +1,26 @@
+#!/usr/pkg/bin/bash
+#
+# unpack NetBSD daily sets
+#
+# depends: common sense
+# work in progress
+
+set -e
+set -x
+
+for file in base comp kern-GENERIC modules xbase xcomp xfont xserver
+do
+	tar zxpf /home/chef/HEAD/$1/${file}.tgz
+done
+
+#tar -zxpf /home/chef/HEAD/$1/kern-GENERIC.tgz
+#ln -fh /netbsd /netbsd.old
+#cp /home/chef/HEAD/$1/netbsd /netbsd.new
+#ln -fh /netbsd.new /netbsd
+
+#for file in etc xetc
+#do
+#	/usr/sbin/postinstall -s ~/${file}.tgz check
+#    /usr/sbin/postinstall -s ~/${file}.tgz fix
+#    /usr/sbin/etcupdate -s ~/${file}.tgz
+#done
