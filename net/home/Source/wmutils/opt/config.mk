107697e7b89f18e808a34335306088647bc8c80f
ChefIronBelly <werthmation@gmail.com>
2018-07-09 08:40:48 -0400

    adds and tweaks

--- net/home/Source/wmutils/opt/config.mk
+++ net/home/Source/wmutils/opt/config.mk
@@ -5,5 +5,4 @@ CC      = cc
 LD      = $(CC)
 
 CFLAGS  = -I/usr/pkg/include -I/usr/X11R7/include -std=c99 -pedantic -Wall -Os
-LDFLAGS = -Wl,-R/usr/X11R7/lib -L/usr/X11R7/lib -lxcb
-
+LDFLAGS = -Wl,-R/usr/X11R7/lib -L/usr/X11R7/lib -lxcb -lxcb-image
