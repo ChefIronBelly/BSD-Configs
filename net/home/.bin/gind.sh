03c49526d8fa2c66fdea46aa18fee81a1f826da3
ChefIronBelly <werthmation@gmail.com>
2018-07-11 17:30:03 -0400

    adds and tweaks

--- net/home/.bin/gind.sh
+++ net/home/.bin/gind.sh
@@ -1,29 +1,30 @@
 #!/bin/sh
 
-FSDIR=${FSDIR:-/tmp/grp}
+FSDIR=${FSDIR:-/tmp/groups}
 group1=""
 group2=""
 group3=""
 group4=""
 
-if [ -f $FSDIR/1 ];
-    then group1="1"
-    else group1="0"
+if [ ! “$(ls -A “$FSDIR/1”)” ];
+    then group1="0"
+    else group1="1"
 fi
 
-if [ -f $FSDIR/2 ];
-    then group2="1"
-    else group2="0"
+if [ ! “$(ls -A “$FSDIR/2”)” ];
+    then group2="0"
+    else group2="1"
 fi
 
-if [ -f $FSDIR/3 ];
-    then group3="1"
-    else group3="0"
+if [ ! “$(ls -A “$FSDIR/3”)” ];
+    then group3="0"
+    else group3="1"
 fi
 
-if [ -f $FSDIR/4 ];
-    then group4="1"
-    else group4="0"
+if [ ! “$(ls -A “$FSDIR/4”)” ];
+    then group4="0"
+    else group4="1"
 fi
 
 echo "$group4""$group3""$group2""$group1"
+
