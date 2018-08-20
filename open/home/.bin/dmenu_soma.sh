5ae89b59bc30d08c808531b5ad1919a55d563e3d
ChefIronBelly <werthmation@gmail.com>
2018-08-20 17:08:30 -0400

    adds and tweaks

--- open/home/.bin/dmenu_soma.sh
+++ open/home/.bin/dmenu_soma.sh
@@ -6,17 +6,17 @@
 selection="";
 
 if [ "$(pgrep mpg123)" ] ; then
-        echo -n "All your sound belong to us ..." | osd_cat -p top -A center -d 3 -s 0 -c '#cfb9a8' -f $FONT
+        pop.sh "All your sound belong to us ..."
         pkill mpg123
         exit 0
 fi
 
 stations="$(cat /home/chef/.bin/stations)"
 selection="$(echo "$stations" | dmenu "$@" $DMENU_FN $DMENU_NB $DMENU_NF $DMENU_SF $DMENU_SB)"
-player() { mpg123 -C -b 3000 -@ "$@" 2>/dev/null & }
+player() { mpg123 -b 2048 -C -@ "$@" 2>/dev/null & }
 
 if [ "$selection" ]; then
-		echo -n "Playing SomaFM channel $selection ..." | osd_cat -p top -A center -d 3 -s 0 -c '#cfb9a8' -f $FONT
+		pop.sh "Playing SomaFM channel $selection ..."
 		player http://somafm.com/$selection.pls
 else exit;
 fi
