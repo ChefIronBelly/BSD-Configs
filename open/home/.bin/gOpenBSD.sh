7dbdc8848e9ea647895e9b959d527053a2710c09
ChefIronBelly <werthmation@gmail.com>
2018-08-22 12:18:29 -0400

    adds and tweaks

--- open/home/.bin/gOpenBSD.sh
+++ open/home/.bin/gOpenBSD.sh
@@ -1,5 +1,11 @@
-#!/bin/sh
+#!/usr/local/bin/bash
 #
 # Get OpenBSD snapshots
 #
+set -e
+set -x
+
+now=$(date +"%m.%d")
+mkdir /home/chef/snapshots/${now} && cd $_
+
 ftp -ia ftp://mirror.csclub.uwaterloo.ca/pub/OpenBSD/snapshots/amd64/{index.txt,*tgz,bsd*,INS*,BOOT*,BUILDINFO,SHA*}
