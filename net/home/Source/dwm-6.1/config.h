074a1e65144f91ffb8a36ea937c03d7586ff5a27
ChefIronBelly <werthmation@gmail.com>
2018-08-22 07:50:41 -0400

    adds and tweaks

--- net/home/Source/dwm-6.1/config.h
+++ net/home/Source/dwm-6.1/config.h
@@ -58,11 +58,15 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_start.sh", NULL };
 static const char *termcmd[]  = { "xst", NULL };
+static const char *surfcmd[]  = { "firefox60", NULL };
+static const char *editcmd[]  = { "geany", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY|ShiftMask,             XK_d,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY|ShiftMask,             XK_f, 	   spawn,          {.v = surfcmd } },
+	{ MODKEY|ShiftMask,             XK_g, 	   spawn,          {.v = editcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
