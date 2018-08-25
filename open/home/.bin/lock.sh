daba025292a7fd740a74fd18d94100cecf929801
ChefIronBelly <werthmation@gmail.com>
2018-08-25 06:46:25 -0400

    adds and tweaks

--- open/home/.bin/lock.sh
+++ open/home/.bin/lock.sh
@@ -1,8 +1,6 @@
-#!/bin/mksh
-TMPBG=/tmp/screen.png
-LOCK=$HOME/Pictures/lock.png
-RES=$(xrandr | grep '*' | sed -E "s/[^0-9]*([0-9]+)x([0-9]+).*/\1*\2/")
- 
-ffmpeg -f x11grab -video_size $RES -y -i $DISPLAY -i $LOCK -filter_complex "boxblur=5:1,overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" -vframes 1 $TMPBG -loglevel quiet
-xlock feh $TMPBG
-rm $TMPBG
+#!/bin/sh
+# turn screen off and lock X
+
+xset dpms force off
+
+slock "$@"
