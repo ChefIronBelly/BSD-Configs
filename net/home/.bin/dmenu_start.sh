4ca532191fc7a070c8e2082a0ce31349de8559cd
ChefIronBelly <werthmation@gmail.com>
2018-08-31 15:49:09 -0400

    adds and tweaks

--- net/home/.bin/dmenu_start.sh
+++ net/home/.bin/dmenu_start.sh
@@ -2,4 +2,7 @@
 
 . $HOME/.wmrc
 
-dmenu_run -i $DMENU_FN $DMENU_NB $DMENU_NF $DMENU_SF $DMENU_SB
+dmenu_run -i -p 'Search' $DMENU_FN $DMENU_NB $DMENU_NF $DMENU_SF $DMENU_SB
+
+#dmenu_run -b -i -p 'Search' -fn 'Menlo:size=10' -w '360' -h '20' -x '24' -y '48' -dim '0.4'
+
