34de24362e05ae106c2eb5eae09577024ddabed9
ChefIronBelly <werthmation@gmail.com>
2018-10-02 08:56:42 -0400

    adds and tweaks

--- net/home/.bin/gNetBSD8.sh
+++ net/home/.bin/gNetBSD8.sh
@@ -9,7 +9,7 @@ set -x
 
 mkdir /home/chef/netbsd-8/$1 && cd $_
 
-for file in kern-GENERIC modules base comp etc xbase xcomp xetc xfont xserver
+for file in kern-GENERIC modules base comp etc man text xbase xcomp xetc xfont xserver
 do
-    curl -LO http://nyftp.netbsd.org/pub/NetBSD-daily/netbsd-8/$1/amd64/binary/sets/${file}.tgz
+    curl -C - -LO http://nyftp.netbsd.org/pub/NetBSD-daily/netbsd-8/latest/amd64/binary/sets/${file}.tgz
 done
